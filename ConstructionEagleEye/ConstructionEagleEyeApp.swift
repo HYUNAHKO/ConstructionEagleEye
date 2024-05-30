d0029edacdb89b310b3b4d240da47d66a9f0bd89
HYUNAHKO <hy.na311@gmail.com>
2024-05-30 14:46:21 +0900

    Initial Commit

---
+++ ConstructionEagleEye/ConstructionEagleEyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConstructionEagleEyeApp.swift
+//  ConstructionEagleEye
+//
+//  Created by snlcom on 5/30/24.
+//
+
+import SwiftUI
+
+@main
+struct ConstructionEagleEyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
